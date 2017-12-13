62d9a4d2ffb114dcea4c71c867149628c0e33ce7
Andrew N <andrew@gingerbreadshed.com>
2017-12-13 16:43:08 -0700

    more boilerplate

---
+++ server/manifest.js
@@ -0,0 +1,65 @@
+const Boom = require('boom');
+const packageModule = require('./package');
+const config = require('./config');
+
+const registrations = [
+	{
+		plugin: {
+			register: 'hapi-auth-jwt2'
+		}
+	},
+	{
+		plugin: {
+			register: './lib/routes'
+		}
+	},
+	{
+		plugin: {
+			register: 'good',
+			options: {
+				reporters: {
+					consoleReporter: [
+						{ module: 'good-squeeze', name: 'Squeeze', args: [{ log: '*', response: '*', error: '*', request: '*' }] },
+						{ module: 'good-console' },
+						'stdout'
+					]
+				}
+			}
+		}
+	}
+];
+
+module.exports = {
+	server: {
+		debug: { log: ['error'], request: ['error'] },
+		connections: {
+			routes: {
+				validate: {
+					options: { abortEarly: false },
+					failAction: (request, reply, source, error) => {
+						if (!error.data || !error.data.details) {
+							if (error.isBoom) {
+								return reply(error);
+							} else {
+								return reply(Boom.badImplementation(error));
+							}
+						}
+						error.output.payload.validationErrors = error.data.details.map(failure => ({
+							message: failure.message,
+							type: failure.type,
+							key: failure.path,
+							data: failure.data
+						}));
+						reply(error);
+					}
+				}
+			}
+		}
+	},
+	connections: [
+		{
+			port: 9000
+		}
+	],
+	registrations
+};
---
+++ server/server.js
@@ -0,0 +1,17 @@
+const Glue = require('glue');
+const manifest = require('./manifest');
+
+const options = {
+	relativeTo: __dirname + '/src'
+};
+
+Glue.compose(manifest, options, function(err, server) {
+	if (err) {
+		throw err;
+	}
+
+	server.start(function() {
+		console.log('Server running at:', server.info.uri);
+		console.log('Environment:', process.env.NODE_ENV);
+	});
+});
